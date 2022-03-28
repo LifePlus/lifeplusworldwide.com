ec9148f9e48383034f265f413788b81ca3a9eabe
Grant Holle <hollegrant@gmail.com>
2022-03-28 15:24:48 +0400

    Add brand colors to config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,7 +4,27 @@ module.exports = {
     './src/**/*.{astro,js,jsx,svelte,ts,tsx,vue}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brand: {
+          primary: '#E30613',
+          base: '#FFFCE5',
+          pink: '#E60064',
+          gold: '#A89258',
+          blue: '#323044',
+          green: '#ADA01F',
+          purple: '#702283',
+          brown: '#932826',
+          orange: '#E84E0F',
+        },
+        gray: {
+          300: '#CCCCCC',
+          500: '#999999',
+          700: '#666666',
+          900: '#333333',
+        }
+      }
+    },
   },
   plugins: [],
 }
