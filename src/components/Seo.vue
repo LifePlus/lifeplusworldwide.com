b28a953f9adbc82d17a635e2c4a0ddd5c793f80b
Grant Holle <hollegrant@gmail.com>
2022-03-30 08:56:27 +0400

    WIP page generator

---
+++ src/components/Seo.vue
@@ -0,0 +1,21 @@
+<template>
+  <component
+    :is="seoTag.tag"
+    v-bind="seoTag.attributes || {}"
+  >
+    {{ seoTag.content }}
+  </component>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  props: {
+    seoTag: Object,
+  },
+  setup () {
+
+  },
+})
+</script>
