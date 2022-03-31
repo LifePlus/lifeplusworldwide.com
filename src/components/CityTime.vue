8489a26d10f82518ec248c84137e48569234e1dd
Grant Holle <hollegrant@gmail.com>
2022-03-31 13:54:17 +0400

    Add a city time component

---
+++ src/components/CityTime.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="text-xs">
+    <div class="font-bold">
+      <slot />
+    </div>
+    <div class="whitespace-no-wrap">10 : 48 : 36</div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    timezone: String,
+  },
+  setup() {
+
+  },
+}
+</script>
