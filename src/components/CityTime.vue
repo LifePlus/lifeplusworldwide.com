cd1fba8ce8ed624c2d28cb2e85b04d522ea2cccc
Grant Holle <hollegrant@gmail.com>
2022-04-01 10:06:19 +0400

    Get clocks ticking

--- src/components/CityTime.vue
+++ src/components/CityTime.vue
@@ -1,19 +1,48 @@
 <template>
   <div class="text-xs">
     <div class="font-bold">
-      <slot />
+      {{ label }}
     </div>
-    <div class="whitespace-no-wrap">10 : 48 : 36</div>
+    <div class="whitespace-no-wrap font-mono">{{ time }}</div>
   </div>
 </template>
 
 <script>
+import { ref, onBeforeUnmount } from 'vue'
+import dateFromTz from 'date-from-timezone'
+import leftPad from 'just-left-pad'
+
 export default {
   props: {
-    timezone: String,
+    timezone: {
+      type: String,
+      required: true,
+    },
+    label: {
+      type: String,
+      required: true,
+    }
   },
-  setup() {
+  setup ({ timezone }) {
+    const dateFn = dateFromTz(timezone)
+    const formatTime = date => {
+      return `${leftPad(date.getHours().toString(), 2, '0')}:${leftPad(date.getMinutes().toString(), 2, '0')}:${leftPad(date.getSeconds().toString(), 2, '0')}`
+    }
+    const getFormattedTime = () => {
+      return formatTime(dateFn(Date.now()))
+    }
+    const time = ref(getFormattedTime())
+    let interval = setInterval(() => {
+      time.value = getFormattedTime()
+    }, 1000)
 
+    onBeforeUnmount(() => {
+      clearInterval(interval)
+    })
+
+    return {
+      time,
+    }
   },
 }
 </script>
