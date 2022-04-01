8f3d3a1c0326512a4e8883fbc0efb653f4c2e512
Grant Holle <hollegrant@gmail.com>
2022-04-01 11:18:41 +0400

    Make a round button component

---
+++ src/components/RoundButton.vue
@@ -0,0 +1,5 @@
+<template>
+  <button class="w-16 h-16 mx-auto flex items-center justify-center rounded-full border border-brand-primary bg-brand-base hover:bg-brand-primary hover:text-brand-base transition">
+    <slot />
+  </button>
+</template>
