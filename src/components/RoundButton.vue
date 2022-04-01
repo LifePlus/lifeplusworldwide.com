ed0407752a867644b9a161b0f0c0b1553c5acde2
Grant Holle <hollegrant@gmail.com>
2022-04-01 14:52:44 +0400

    WIP masthead

--- src/components/RoundButton.vue
+++ src/components/RoundButton.vue
@@ -1,5 +1,20 @@
 <template>
-  <button class="w-16 h-16 mx-auto flex items-center justify-center rounded-full border border-brand-primary bg-brand-base hover:bg-brand-primary hover:text-brand-base transition">
+  <button
+    class="w-16 h-16 mx-auto flex items-center justify-center rounded-full border border-brand-primary transition"
+    :class="{
+      'bg-brand-base hover:bg-brand-primary hover:text-brand-base': !solid,
+      'bg-brand-primary text-white': solid,
+    }"
+  >
     <slot />
   </button>
 </template>
+
+<script>
+export default {
+  props: {
+    solid: Boolean,
+  },
+  setup () {}
+}
+</script>
