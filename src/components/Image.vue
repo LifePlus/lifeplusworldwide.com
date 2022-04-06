537263f13e0d48d82bb6aff6d20b1d94e94f8016
Grant Holle <hollegrant@gmail.com>
2022-04-06 13:45:25 +0400

    Try something special

---
+++ src/components/Image.vue
@@ -0,0 +1,16 @@
+<template>
+  <Image :data="data" :explicit-width="false" />
+</template>
+
+<script>
+import * as pkg from 'vue-datocms'
+
+export default {
+  components: {
+    Image: pkg.Image,
+  },
+  props: {
+    data: Object,
+  }
+}
+</script>
