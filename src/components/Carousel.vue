8493cf9e5ec91c3bbb04c115c86927d19361261e
Grant Holle <hollegrant@gmail.com>
2022-04-06 09:21:20 +0400

    Improve quote carousel

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -1,30 +1,31 @@
 <template>
   <div>
-    <pre>{{ carousel?.data }}</pre>
     <Carousel
-      :items-to-show="1.75"
-      wrap-around
+      :settings="settings"
+      :breakpoints="breakpoints"
       ref="carousel"
+      @update:modelValue="value => currentSlide = value"
     >
       <template #default="{}">
         <Slide
           v-for="(quote, index) in quotesComputed"
           :key="quote.id"
+          :index="index"
         >
-          <div class="max-w-[34rem]">
+          <div class="max-w-[34rem] px-16 md:px-0">
             <div
               v-html="quote.quote"
-              class="pt-3 pb-6 border-t text-4xl text-left transition"
+              class="pt-3 pb-6 border-t text-2xl md:text-4xl text-left transition"
               :class="{
-                'border-brand-primary text-black': index === carousel?.data?.currentSlide,
-                'border-transparent text-gray-300': index !== carousel?.data?.currentSlide,
+                'border-brand-primary text-black': index === currentSlide,
+                'border-transparent text-gray-300': index !== currentSlide,
               }"
             />
             <div
-              class="text-2xl py-3 border-y text-left transition"
+              class="text-xl md:text-2xl py-3 border-y text-left transition"
               :class="{
-                'border-brand-primary text-black': index === carousel?.data?.currentSlide,
-                'border-transparent text-gray-300': index !== carousel?.data?.currentSlide,
+                'border-brand-primary text-black': index === currentSlide,
+                'border-transparent text-gray-300': index !== currentSlide,
               }"
             >{{ quote.author }}</div>
           </div>
@@ -32,8 +33,8 @@
       </template>
 
       <template #addons>
-        <div class="absolute max-w-[42rem] w-full mx-auto flex justify-between items-center inset-0">
-          <img class="absolute left -translate-x-full" :src="illustration.responsiveImage.src" :alt="illustration.alt" />
+        <div class="absolute max-w-[42rem] px-4 md:px-0 w-full mx-auto flex justify-between items-center inset-0">
+          <img class="hidden lg:block absolute left -translate-x-full" :src="illustration.responsiveImage.src" :alt="illustration.alt" />
           <button @click="carousel.prev()" class="w-8 h-8 flex items-center justify-center rounded-full bg-brand-primary text-white">
             <ArrowLeftIcon class="w-4 h-4" />
           </button>
@@ -47,7 +48,7 @@
 </template>
 
 <script>
-import { ref, computed, onMounted } from 'vue'
+import { ref, computed, onMounted, nextTick } from 'vue'
 import 'vue3-carousel/dist/carousel.css'
 import { Carousel, Slide } from 'vue3-carousel'
 import ArrowRightIcon from './icons/ArrowRightIcon.vue'
@@ -73,12 +74,40 @@ export default {
         ...props.quotes,
       ]
     })
-    const currentSlide = ref()
+    const currentSlide = ref(0)
+    const settings = {
+      wrapAround: true,
+      itemsToShow: 1,
+      snapAlign: 'center'
+      autoplay: true,
+    }
+    const breakpoints = {
+      768: {
+        itemsToShow: 1.25
+      },
+      1024: {
+        itemsToShow: 1.75
+      },
+      1280: {
+        itemsToShow: 2
+      },
+      1536: {
+        itemsToShow: 2.25
+      },
+    }
+
+    onMounted(() => {
+      nextTick(() => {
+        // currentSlide.value = carousel.value?.data?.middleSlide.value + 1
+      })
+    })
 
     return {
       carousel,
       currentSlide,
       quotesComputed,
+      settings,
+      breakpoints,
     }
   },
 }
