fd202d8271c2fccc3b2862424b9371464f306a0c
Grant Holle <hollegrant@gmail.com>
2022-04-15 14:50:09 +0400

    Add contact form type

---
+++ src/components/ContactForm.vue
@@ -0,0 +1,100 @@
+<template>
+  <form @submit.prevent="send">
+    <div>
+      <p class="font-medium text-sm mb-2">Who can best help you with your question? <span class="text-red-600">*</span></p>
+      <div class="text-gray-700 space-y-2 mb-6">
+        <div class="flex items-center">
+          <input v-model="data.question" value="Employment Inquiries" id="enquiries" name="question" type="radio" class="focus:ring-brand-purple h-4 w-4 text-brand-purple border-gray-300" />
+          <label for="enquiries" class="ml-3 block text-sm text-black">
+            Employment Inquiries
+          </label>
+        </div>
+        <div class="flex items-center">
+          <input v-model="data.question" value="Donor Opportunities" id="opps" name="question" type="radio" class="focus:ring-brand-purple h-4 w-4 text-brand-purple border-gray-300" />
+          <label for="opps" class="ml-3 block text-sm text-black">
+            Donor Opportunities
+          </label>
+        </div>
+        <div class="flex items-center">
+          <input v-model="data.question" value="US Employment" id="us-emp" name="question" type="radio" class="focus:ring-brand-purple h-4 w-4 text-brand-purple border-gray-300" />
+          <label for="us-emp" class="ml-3 block text-sm text-black">
+            US Employment
+          </label>
+        </div>
+        <div class="flex items-center">
+          <input v-model="data.question" value="Other Details Needed" id="other-details" name="question" type="radio" class="focus:ring-brand-purple h-4 w-4 text-brand-purple border-gray-300" />
+          <label for="other-details" class="ml-3 block text-sm text-black">
+            Other Details Needed
+          </label>
+        </div>
+      </div>
+    </div>
+
+    <div class="">
+      <div class="mb-1">
+        <label class="font-medium text-sm" for="name">What's your name? <span class="text-red-600">*</span></label>
+      </div>
+      <div class="w-full lg:flex-1 mb-6">
+        <AppInput v-model="data.name" id="name" placeholder="Jane Smith" />
+      </div>
+    </div>
+    <div class="">
+      <div class="mb-1">
+        <label class="font-medium text-sm" for="email">What's your email? <span class="text-red-600">*</span></label>
+      </div>
+      <div class="w-full lg:flex-1 mb-6">
+        <AppInput v-model="data.email" type="email" id="email" placeholder="jane@gmail.com" />
+      </div>
+    </div>
+    <div class="">
+      <div class="mb-1">
+        <label class="font-medium text-sm" for="phone">What's your phone number?</label>
+      </div>
+      <div class="w-full lg:flex-1 mb-6">
+        <AppInput v-model="data.phone" type="tel" id="phone" placeholder="########" />
+      </div>
+    </div>
+
+    <div>
+      <button class="inline-flex items-center space-x-4 text-lg lg:text-2xl font-bold">
+        <span>Send enquiry...</span>
+        <div class="flex-1 h-12 w-12 mx-auto flex items-center justify-center rounded-full bg-brand-primary text-white transition">
+          <ArrowRightIcon class="w-5 h-5 fill-current" />
+        </div>
+      </button>
+    </div>
+  </form>
+</template>
+
+<script>
+import { ref, reactive } from 'vue'
+import axios from 'axios'
+import AppInput from '../components/forms/AppInput.vue'
+import ArrowRightIcon from './icons/ArrowRightIcon.vue'
+
+export default {
+  components: {
+    AppInput,
+    ArrowRightIcon,
+  },
+
+  setup () {
+    const loading = ref(false)
+    const data = reactive({
+      question: '',
+      name: '',
+      email: '',
+      phone: '',
+    })
+    const send = async () => {
+
+    }
+
+    return {
+      data,
+      send,
+      loading,
+    }
+  },
+}
+</script>
---
+++ src/components/forms/AppInput.vue
@@ -0,0 +1,3 @@
+<template>
+  <input type="text" class="block px-3 py-1.5 w-full shadow-sm focus:ring-brand-purple focus:border-brand-purple focus:outline-none border border-gray-300 rounded-md" />
+</template>
