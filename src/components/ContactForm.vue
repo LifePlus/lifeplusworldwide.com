226b59b02ef32ac346629796e75b7f0d39407a2a
Grant Holle <hollegrant@gmail.com>
2022-04-18 08:35:49 +0400

    Increase text size

--- src/components/ContactForm.vue
+++ src/components/ContactForm.vue
@@ -1,62 +1,62 @@
 <template>
-  <form @submit.prevent="send">
+  <form @submit.prevent="send" class="text-lg lg:text-2xl space-y-6 lg:space-y-8">
     <div>
-      <p class="font-medium text-sm mb-2">Who can best help you with your question? <span class="text-red-600">*</span></p>
+      <p class="font-medium mb-2">Who can best help you with your question? <span class="text-red-600">*</span></p>
       <div class="text-gray-700 space-y-2 mb-6">
         <div class="flex items-center">
           <input v-model="data.question" value="Employment Inquiries" id="enquiries" name="question" type="radio" class="focus:ring-brand-purple h-4 w-4 text-brand-purple border-gray-300" />
-          <label for="enquiries" class="ml-3 block text-sm text-black">
+          <label for="enquiries" class="ml-3 block text-black">
             Employment Inquiries
           </label>
         </div>
         <div class="flex items-center">
           <input v-model="data.question" value="Donor Opportunities" id="opps" name="question" type="radio" class="focus:ring-brand-purple h-4 w-4 text-brand-purple border-gray-300" />
-          <label for="opps" class="ml-3 block text-sm text-black">
+          <label for="opps" class="ml-3 block text-black">
             Donor Opportunities
           </label>
         </div>
         <div class="flex items-center">
           <input v-model="data.question" value="US Employment" id="us-emp" name="question" type="radio" class="focus:ring-brand-purple h-4 w-4 text-brand-purple border-gray-300" />
-          <label for="us-emp" class="ml-3 block text-sm text-black">
+          <label for="us-emp" class="ml-3 block text-black">
             US Employment
           </label>
         </div>
         <div class="flex items-center">
           <input v-model="data.question" value="Other Details Needed" id="other-details" name="question" type="radio" class="focus:ring-brand-purple h-4 w-4 text-brand-purple border-gray-300" />
-          <label for="other-details" class="ml-3 block text-sm text-black">
+          <label for="other-details" class="ml-3 block text-black">
             Other Details Needed
           </label>
         </div>
       </div>
     </div>
 
-    <div class="">
+    <div>
       <div class="mb-1">
-        <label class="font-medium text-sm" for="name">What's your name? <span class="text-red-600">*</span></label>
+        <label class="font-medium" for="name">What's your name? <span class="text-red-600">*</span></label>
       </div>
-      <div class="w-full lg:flex-1 mb-6">
+      <div>
         <AppInput v-model="data.name" id="name" placeholder="Jane Smith" />
       </div>
     </div>
-    <div class="">
+    <div>
       <div class="mb-1">
-        <label class="font-medium text-sm" for="email">What's your email? <span class="text-red-600">*</span></label>
+        <label class="font-medium" for="email">What's your email? <span class="text-red-600">*</span></label>
       </div>
-      <div class="w-full lg:flex-1 mb-6">
+      <div>
         <AppInput v-model="data.email" type="email" id="email" placeholder="jane@gmail.com" />
       </div>
     </div>
-    <div class="">
+    <div>
       <div class="mb-1">
-        <label class="font-medium text-sm" for="phone">What's your phone number?</label>
+        <label class="font-medium" for="phone">What's your phone number?</label>
       </div>
-      <div class="w-full lg:flex-1 mb-6">
+      <div>
         <AppInput v-model="data.phone" type="tel" id="phone" placeholder="########" />
       </div>
     </div>
 
     <div>
-      <button class="inline-flex items-center space-x-4 group text-lg lg:text-2xl font-bold">
+      <button class="inline-flex items-center space-x-4 group font-bold">
         <span>Send enquiry...</span>
         <div class="flex-1 h-12 w-12 mx-auto flex items-center justify-center rounded-full bg-brand-primary text-white transition-transform group-hover:translate-x-2">
           <ArrowRightIcon class="w-5 h-5 fill-current" />
--- src/components/forms/AppInput.vue
+++ src/components/forms/AppInput.vue
@@ -1,3 +1,3 @@
 <template>
-  <input type="text" class="block px-3 py-1.5 w-full shadow-sm focus:ring-brand-purple focus:border-brand-purple focus:outline-none border border-gray-300 rounded-md" />
+  <input type="text" class="block text-lg lg:text-2xl px-3 py-1.5 w-full shadow-sm focus:ring-brand-purple focus:border-brand-purple focus:outline-none border border-gray-300 rounded-md" />
 </template>
