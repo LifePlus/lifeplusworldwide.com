9e31089b93a96eb0b97383fa211a9ff4332f0441
Grant Holle <hollegrant@gmail.com>
2022-04-26 08:10:21 +0400

    Move arrows instead of scale

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -34,10 +34,10 @@
       <template #addons>
         <div class="absolute max-w-[42rem] px-4 md:px-0 w-full mx-auto flex justify-between items-center inset-0">
           <img v-if="illustration" class="hidden lg:block absolute left -translate-x-full w-[220px]" :srcset="illustration.responsiveImage.webpSrcSet" :src="illustration.responsiveImage.src" :alt="illustration.alt" />
-          <button @click="carousel.prev()" class="transition-transform hover:scale-110 w-8 h-8 flex items-center justify-center rounded-full bg-brand-primary text-white">
+          <button @click="carousel.prev()" class="transition-transform hover:-translate-x-2 duration-200 w-8 h-8 flex items-center justify-center rounded-full bg-brand-primary text-white">
             <ArrowLeftIcon class="w-4 h-4" />
           </button>
-          <button @click="carousel.next()" class="transition-transform hover:scale-110 w-8 h-8 flex items-center justify-center rounded-full bg-brand-primary text-white">
+          <button @click="carousel.next()" class="transition-transform hover:translate-x-2 duration-200 w-8 h-8 flex items-center justify-center rounded-full bg-brand-primary text-white">
             <ArrowRightIcon class="w-4 h-4" />
           </button>
         </div>
