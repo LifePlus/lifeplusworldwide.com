22329d490d37dc564acb3088539302816e8904c9
Grant Holle <hollegrant@gmail.com>
2023-07-17 18:43:14 +0400

    Use secure

--- netlify/functions/submit-referral.js
+++ netlify/functions/submit-referral.js
@@ -19,7 +19,7 @@ exports.handler = (event, context, callback) => {
     transporter = nodemailer.createTransport({
       host: process.env.EMAIL_HOST,
       port: process.env.EMAIL_PORT,
-      secure: false,
+      secure: true,
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASSWORD
