25b2187308e22de15a74720ca611ebf48c9dfac0
Grant Holle <hollegrant@gmail.com>
2023-11-06 14:52:51 +0400

    Update vacancy card

--- src/components/Vacancies.vue
+++ src/components/Vacancies.vue
@@ -1,11 +1,11 @@
 <template>
   <ul v-if="vacancies.length > 0" class="flex-1 self-start w-full relative z-10 min-h-[12rem] divide-y divide-gray-300">
     <li v-for="vacancy in vacancies" :key="vacancy.id" class="py-5 bg-white">
-      <div class="text-sm">{{ vacancy.category }}</div>
+      <div class="text-sm">{{ vacancy.department?.name }}</div>
       <div>
-        <a class="text-2xl font-bold text-brand-primary hover:underline" :href="`https://careers.lifeplusworldwide.com/jobs/${vacancy.id}?utm_source=lifeplus&utm_content=vacancy+list`" target="_blank">{{ vacancy.name.replace(' -', ',') }}</a>
+        <a class="text-2xl font-bold text-brand-primary hover:underline" :href="`${vacancy.page_url}?utm_source=lifeplus&utm_content=vacancy+list`" target="_blank">{{ vacancy.display }}</a>
       </div>
-      <div class="text-sm">Added on {{ vacancy.published_at }}</div>
+      <div class="text-sm">Added on {{ vacancy.added }}</div>
     </li>
   </ul>
 </template>
@@ -21,7 +21,7 @@ export default {
 
   setup (props) {
     const vacancies = ref([])
-    axios.get(`https://apply.lifeplusworldwide.com/api/vacancies?entities[0]=${props.id}&only[0]=name&only[1]=id&only[2]=published_at&only[3]=category`).then(res => {
+    axios.get(`https://vacancies.lifepluslearning.com/api/public-vacancies?entity${props.id}`).then(res => {
       vacancies.value = res.data
     })
 
